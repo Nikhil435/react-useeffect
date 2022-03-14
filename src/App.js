4445842601b933178a7f7090d594d0f1ca1017c2
Nikhil435 <nikhilsainimtv@gmail.com>
2022-03-14 15:18:17 +0530

    document-title-when-rerendeer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ export default function App() {
   const [count, setCount] = useState(0);
   useEffect(() => {
     console.log('use-effect run');
+    document.title = count;
   });
   console.log('compoent-render');
   return (
@@ -20,4 +21,8 @@ export default function App() {
 1. whenever compoent render useeffect callback run everytime
 decide when you want to call useeffect to prevent infinite loop
 2. useeffect run every single time when you compoent render and rerender.
+*/
+
+/*
+logic change
 */
