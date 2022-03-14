7fcca645b73ba45df901219f6245933b931a4d6b
Nikhil435 <nikhilsainimtv@gmail.com>
2022-03-14 15:12:25 +0530

    add-use-effect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './style.css';
 
 export default function App() {
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    console.log('use-effect run');
+  });
+  console.log('compoent-render');
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
+      <button onClick={() => setCount(count + 1)}>{count}</button>
     </div>
   );
 }
+
+/*
+1. whenever compoent render useeffect callback run everytime
+decide when you want to call useeffect to prevent infinite loop
+2.
+*/
