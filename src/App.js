11d8980d8c1ddd0de57baf673201060141429ea0
Nikhil435 <nikhilsainimtv@gmail.com>
2022-03-14 15:25:26 +0530

    issue-with-current-cod-useffect-i-click-ti-run-useeffect-but-i-want-this-for-only-count

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './style.css';
 
 export default function App() {
   const [count, setCount] = useState(0);
+  const [datrMode, setDardMode] = useState(false);
   useEffect(() => {
     console.log('use-effect run');
     document.title = count;
@@ -12,7 +13,10 @@ export default function App() {
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
-      <button onClick={() => setCount(count + 1)}>{count}</button>
+      <button onClick={() => setCount((state) => state + 1)}>{count}</button>
+      <button onClick={() => setDardMode((state) => !state)}>
+        {'toggle DarkMode'}
+      </button>
     </div>
   );
 }
@@ -25,4 +29,9 @@ decide when you want to call useeffect to prevent infinite loop
 
 /*
 logic change
+*/
+
+/*
+there is miner issue with this method
+let's we introduce a new state dardmode
 */
